a4ac6f501f4267a823cdad0b033d78464d9b8aac
cooper <pbg0205@naver.com>
2022-03-22 21:58:54 +0900

    feat: 레시피2-1. 주석 추가

--- ch02-spring-core/chapter02-1/src/main/java/com/cooper/chapter021/Chapter021Application.java
+++ ch02-spring-core/chapter02-1/src/main/java/com/cooper/chapter021/Chapter021Application.java
@@ -8,14 +8,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
-
+//ComponentScan customizing
 @ComponentScan(
         includeFilters = {
                 @ComponentScan.Filter( // 범위를 지정해서 원하는 빈만 스캔할 수 있음
                         type = FilterType.REGEX,
                         pattern = {
                                 "com.cooper.chapter021.component.*Dao",
-                                "com.cooper.chapter021.config.SequenceGeneratorConfiguration"
+                                "com.cooper.chapter021.config.SequenceGeneratorConfiguration",
                         }
                 ),
         },
@@ -29,19 +29,21 @@ import org.springframework.context.annotation.FilterType;
 public class Chapter021Application {
 
     public static void main(String[] args) {
+
+        // 1. Configuration 방식
         ApplicationContext applicationContext1 = new AnnotationConfigApplicationContext(SequenceGeneratorConfiguration.class);
         SequenceGenerator generator = (SequenceGenerator) applicationContext1.getBean("sequenceGenerator");
 
         System.out.println(generator.getSequence());
         System.out.println(generator.getSequence());
 
+        // 2. ComponentScan 방식
         AnnotationConfigApplicationContext applicationContext2
                 = new AnnotationConfigApplicationContext("com.cooper.chapter021.component");
         SequenceDao sequenceDao = applicationContext2.getBean(SequenceDao.class);
 
         System.out.println(sequenceDao.getNextValue("IT"));
         System.out.println(sequenceDao.getNextValue("IT"));
-
     }
 
 }
